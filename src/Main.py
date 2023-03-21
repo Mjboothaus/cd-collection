ecfcdde3e34afcd3c54151502991d30819248a50
Michael J. Booth <mjboothaus@gmail.com>
2023-03-21 05:01:23 +0000

    Fixes for 1.20 Streamlit

--- src/Main.py
+++ src/Main.py
@@ -5,21 +5,27 @@ import streamlit as st
 from helper_functions import read_render_markdown_file, read_toml_file
 from sidebar import create_sidebar
 
-config = read_toml_file()
+APP_TITLE = "CD-Collection"
 
-APP_TITLE = config["st-app"]["APP_TITLE"]
-SUB_TITLE = config["st-app"]["SUB_TITLE"]
+# Note this must be the first function call in the Streamlit app code
 
 st.set_page_config(
     page_title=APP_TITLE,
+    page_icon="🧊",
     layout="wide",
+    initial_sidebar_state="expanded",
     menu_items={
+        'Get Help': 'https://www.databooth.com.au/help',
+        'Report a bug': "https://www.databooth.com.au/bug",
         "About": "Created with love & care at DataBooth - www.databooth.com.au"
     },
 )
 
+config = read_toml_file()
 
-def create_app_header(app_title, subtitle=None):
+SUB_TITLE = config["st-app"]["SUB_TITLE"]
+
+def create_app_header(app_title, subtitle=SUB_TITLE):
     st.header(app_title)
     if subtitle is not None:
         st.subheader(subtitle)
